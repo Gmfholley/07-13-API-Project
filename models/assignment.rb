1734cc9d93d3362cfd0f6bf34aa1cb256c06506f
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-13 12:45:09 -0500

    Creates assignment class/momdel

---
+++ models/assignment.rb
@@ -0,0 +1,45 @@
+class Assignment
+  
+  include DatabaseConnector
+  
+  attr_accessor :name, :description, :where_stored
+  attr_reader :errors, :id
+  
+  def initialize(args={})
+    @id = args[:id] || args["id"]
+    @name = args[:name] || args["name"]
+    @description = args[:description] || args["description"]
+    @where_stored = args[:where_stored] || args["where_stored"]
+    @errors = []
+  end
+  
+  # get the Array of Collaborator objects for this Assignment
+  #
+  # returns an Array
+  def collaborators
+    Collaborator.where_match("assignment_id", id, "==")
+  end
+  
+  # get the Array of Collaborator objects for this Assignment
+  #
+  # returns an Array
+  def links
+    Link.where_match("assignment_id", id, "==")
+  end
+  
+  # returns a Boolean indicating if there are any collaborators
+  #
+  # returns Boolean
+  def collaborators?
+    collabors.length > 0
+  end
+  
+  # returns a Boolean indicating if there are any links
+  #
+  # returns Boolean
+  def links?
+    links.length > 0
+  end
+  
+  
+end
