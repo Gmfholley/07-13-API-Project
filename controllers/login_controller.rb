d910ecf280de82b7002bf6745d7fb6dff7b3fe51
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-16 12:23:49 -0500

    Had to remove session[:expires] because it is nil
    Refactored checking login code

--- controllers/login_controller.rb
+++ controllers/login_controller.rb
@@ -5,21 +5,7 @@ get "/login" do
 end
 
 post "/login/submit" do
-  @user = User.where_match("email", params["log_in"]["contact_info"], "==").first;
-  binding.pry
-  if BCrypt::Password.new(@user.password) == params["log_in"]["password"]
-    session[:user_id] = @user.id
-    if params["log_in"]["keep_me_logged"] == "on"
-      session[:expires] = Chronic.parse("tomorrow");
-    else
-      session[:expires] = Chronic.parse("four hours");
-    end
-    redirect "/api"
-  else
-    home_menu_nav_variables
-    @message = "That user name and/or password was incorrect.  Please try again."
-    erb :login
-  end
+  check_login(params["log_in"])
 end
 
 
--- controllers/login_helper.rb
+++ controllers/login_helper.rb
@@ -2,22 +2,35 @@ module LoginHelper
 
 
   def login_unsuccessful
-  
+    home_menu_nav_variables
+    @message = "That user name and/or password was incorrect.  Please try again."
+    erb :login
   end
 
-  def login_successful
-
+  def login_successful(params)
+    session[:user_id] = @user.id
+    if params["keep_me_logged"] == "on"
+      session[:expires] = Chronic.parse("tomorrow");
+    else
+      session[:expires] = Chronic.parse("four hours from now");
+    end
+    redirect "/api"
   end
 
-  def check_login
-  
+  def check_login(params)
+    @user = User.where_match("email", params["contact_info"], "==").first;
+    if BCrypt::Password.new(@user.password) == params["password"]
+      login_successful(params)
+    else
+      login_unsuccessful
+    end
   end
   
   # sets the current user or redirects to the login screen
   #
   # returns the current user or redirects
   def current_user
-    if session[:user_id] && session[:expires] > Time.now
+    if session[:user_id] # Do not know how to check for nil session expires.  #TODO - figure out session expires
       @current_user = User.create_from_database(session[:user_id].to_i)
     else
       session.clear
