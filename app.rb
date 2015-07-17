470af3a0b5653439353f6186b18afde30248a2e0
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-17 10:58:35 -0500

    Adds production database configuration for Sinatra

--- app.rb
+++ app.rb
@@ -25,6 +25,20 @@ configure :development do
   ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'assignments.db')
 end
 
+## Sinatra specific 
+configure :production do  
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
 # Database set up modules and helper classes required by models
 
 require_relative 'foreign_key.rb'
